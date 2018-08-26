e978b9db20300f4005ca71b0e06a503c3299f6dc
tharun0597 <tharun347@gmail.com>
2018-08-26 15:38:24 +0530

    Initial-draft

---
+++ javahello/src/main/java/com/tarun/studies/App.java
@@ -0,0 +1,13 @@
+package com.tarun.studies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
